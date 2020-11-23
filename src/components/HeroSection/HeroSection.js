8b030abc65cd8c8dd003084589a323af60889550
Nicolas Corbalan <outreef@hotmail.com>
2020-11-23 17:53:43 +0100

    adding funtionality

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import video from '../../videos/video.mp4';
-import {HeroContainer, HeroBg, VideoBg} from './HeroElements';
+import {Button} from '../ButtonElement'
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements';
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={video} type='video.mp4' />
       </HeroBg> 
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+          <HeroP>
+            Sign up for a new account today and receive $250 in credit towards your next payment.
+          </HeroP>
+          <HeroBtnWrapper>
+            <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+              Get started {hover ? <ArrowForward /> : <ArrowRight />}
+            </Button>
+          </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
