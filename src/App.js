1b16627245f137343e58c468b474bab16cba0676
Nicolas Corbalan <outreef@hotmail.com>
2020-11-23 12:08:12 +0100

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar /> 
     </Router>
   );
