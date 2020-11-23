703866d1a541fff255e3222c53a609efb32fe3a5
Nicolas Corbalan <outreef@hotmail.com>
2020-11-23 12:57:55 +0100

    styling navbar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import {SidebarContainer, Icon, CloseIcon} from './SidebarElements';
+import {
+  SidebarContainer, 
+  Icon, 
+  CloseIcon, 
+  SidebarWrapper, 
+  SidebarMenu, 
+  SidebarLink, 
+  SidebarBtnWrap, 
+  SidebarRoute
+} from './SidebarElements';
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
       <Icon>
         <CloseIcon />
-      </Icon>     
+      </Icon>   
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='about'>
+            About
+          </SidebarLink>
+          <SidebarLink to='discover'>
+            Discover
+          </SidebarLink>
+          <SidebarLink to='services'>
+            Services
+          </SidebarLink>
+          <SidebarLink to='signup'>
+            Sign Up
+          </SidebarLink>
+        </SidebarMenu>
+        <SidebarBtnWrap>
+          <SidebarRoute to='/signin'>
+            Sign In
+          </SidebarRoute>
+        </SidebarBtnWrap>
+      </SidebarWrapper>  
     </SidebarContainer>
   )
 }
